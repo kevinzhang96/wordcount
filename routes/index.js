eba20dc593f100e8dc97e567093bc893e9724ab4
Kevin Zhang <kevin.zhang500@gmail.com>
2016-06-09 13:15:38 -0400

    spam patrick

--- routes/index.js
+++ routes/index.js
@@ -6,26 +6,28 @@ var nodemailer = require("nodemailer");
 var smtpTransport = nodemailer.createTransport("SMTP",{
    service: "Gmail",
    auth: {
-       user: "garagewordcount@gmail.com",
-       pass: "garagewordcount135"
+	   user: "garagewordcount@gmail.com",
+	   pass: "garagewordcount135"
    }
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  smtpTransport.sendMail({
-   from: "Garage Wordcount <garagewordcount@gmail.com>", // sender address
-   to: "Kevin Zhang <kevin.zhang500@gmail.com>", // comma separated list of receivers
-   subject: "Hello ✔", // Subject line
-   text: "Hello world ✔" // plaintext body
-  }, function(error, response){
-    if(error) {
-        console.log(error);
-    } else {
-        console.log("Message sent: " + response.message);
-    }
-  });
-  res.render('index', { title: 'Express' });
+	for (var i = 0; i < 30; i++) {
+		smtpTransport.sendMail({
+		from: "Garage Wordcount <garagewordcount@gmail.com>", // sender address
+		to: "Patrick Pan <patrick.pan@patrickpan.com>", // comma separated list of receivers
+		subject: "Hello ✔", // Subject line
+		text: "Hello world ✔" // plaintext body
+		}, function(error, response){
+			if(error) {
+				console.log(error);
+			} else {
+				console.log("Message sent: " + response.message);   
+			}
+		});
+	}
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
