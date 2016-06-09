965330c521f743087a684b89d261912d9fb3366a
Kevin Zhang <kevin.zhang500@gmail.com>
2016-06-09 12:43:12 -0400

    added nodemailer module and added email to /

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+var nodemailer = require("nodemailer");
+
+var smtpTransport = nodemailer.createTransport("SMTP",{
+   service: "Gmail",
+   auth: {
+       user: "garagewordcount@gmail.com",
+       pass: "garagewordcounter123"
+   }
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  smtpTransport.sendMail({
+   from: "Garage Wordcount <garagewordcount@gmail.com>", // sender address
+   to: "Kevin Zhang <kevin.zhang500@gmail.com>", // comma separated list of receivers
+   subject: "Hello ✔", // Subject line
+   text: "Hello world ✔" // plaintext body
+  }, function(error, response){
+    if(error) {
+        console.log(error);
+    } else {
+        console.log("Message sent: " + response.message);
+    }
+  });
+  smtpTransport.sendMail({
+   from: "Garage Wordcount <garagewordcount@gmail.com>", // sender address
+   to: "Kevin Zhang <kevin.zhang500@gmail.com>", // comma separated list of receivers
+   subject: "Hello ✔", // Subject line
+   text: "Hello world ✔" // plaintext body
+  });
   res.render('index', { title: 'Express' });
 });
 
